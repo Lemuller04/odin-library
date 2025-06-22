634582c90d80ed0cfe04a8cf8bdc18a3cd3b2aed
Leonardo <leonardomullerdelima@pm.me>
2025-06-22 15:01:57 -0300

    feat: add dialog form

--- script.js
+++ script.js
@@ -11,13 +11,22 @@ function addBookToLibrary(title, author, year) {
   myLibrary.push(new Book(title, author, year));
 }
 
+// Creates a table on the page by iterating over the myLibrary array
 function listBooks() {
   const main = document.querySelector(".table-container");
   const table = document.createElement("table");
+  const caption = document.createElement("caption");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
   let keys = Object.keys(myLibrary[0]);
   keys.pop();
 
-  addTableHeader(table, keys);
+  caption.textContent = "List of Books";
+  table.appendChild(caption);
+  table.appendChild(thead);
+  table.appendChild(tbody);
+
+  addTableHeader(thead, keys);
 
   myLibrary.forEach((book) => {
     const tr = document.createElement("tr");
@@ -29,26 +38,30 @@ function listBooks() {
     });
 
     const td = document.createElement("td");
-    td.textContent = "Remove";
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    td.appendChild(removeButton);
     td.id = book.id;
     td.classList.add("remove");
     tr.appendChild(td);
-    table.appendChild(tr);
+    tbody.appendChild(tr);
   });
 
   main.appendChild(table);
 }
 
-function addTableHeader(table, keys) {
+// Defines the table header based on the keys of the Book object
+function addTableHeader(thead, keys) {
   const tr = document.createElement("tr");
 
   keys.forEach((key) => {
     const th = document.createElement("th");
     th.textContent = capitalize(key);
+    th.setAttribute("scope", "col");
     tr.appendChild(th);
   });
 
-  table.appendChild(tr);
+  thead.appendChild(tr);
 }
 
 function capitalize(word) {
@@ -63,10 +76,36 @@ addBookToLibrary("Book 5", "Author 5", 1995);
 
 listBooks();
 
+// Button related code
+const openModal = document.querySelector("[data-open-modal]");
+const closeModal = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-model]");
 const addBookButton = document.querySelector(".add-book");
 let removeBookButtons = document.querySelectorAll(".remove");
 
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+// Allows closing the modal by clicking outside of it
+modal.addEventListener("click", (e) => {
+  const dialogDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    modal.close();
+  }
+});
+
 addBookButton.addEventListener("click", () => {
+  modal.close();
   alert("hi");
 });
 
