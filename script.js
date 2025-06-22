dbec566bea17db56e44e2e491af741c7c584ee9a
Leonardo <leonardomullerdelima@pm.me>
2025-06-22 16:23:15 -0300

    feat: complete add books function

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 const myLibrary = [];
 
-function Book(title, author, year) {
+function Book(title, author, year, read) {
   this.title = title;
   this.author = author;
   this.year = year;
+  this.read = read ? "Read" : "Not Read";
   this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, year) {
-  myLibrary.push(new Book(title, author, year));
+function addBookToLibrary(title, author, year, read = false) {
+  myLibrary.push(new Book(title, author, year, read));
 }
 
 // Creates a table on the page by iterating over the myLibrary array
 function listBooks() {
   const main = document.querySelector(".table-container");
+
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+
   const table = document.createElement("table");
   const caption = document.createElement("caption");
   const thead = document.createElement("thead");
   const tbody = document.createElement("tbody");
-  let keys = Object.keys(myLibrary[0]);
-  keys.pop();
 
   caption.textContent = "List of Books";
   table.appendChild(caption);
   table.appendChild(thead);
   table.appendChild(tbody);
 
+  if (myLibrary.length < 1) {
+    main.textContent = "It looks like the library is empty";
+    return;
+  }
+
+  let keys = Object.keys(myLibrary[0]);
+  keys.pop();
   addTableHeader(thead, keys);
 
   myLibrary.forEach((book) => {
@@ -40,12 +51,25 @@ function listBooks() {
     const td = document.createElement("td");
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
+    removeButton.classList.add("remove");
+    removeButton.id = book.id;
+
+    removeButton.addEventListener("click", () => {
+      let buttonId = removeButton.id;
+      const book = myLibrary.find((book) => book.id === buttonId);
+      const index = myLibrary.indexOf(book);
+      removeButton.parentNode.remove();
+      myLibrary.splice(index, 1);
+      listBooks();
+    });
+
     td.appendChild(removeButton);
-    td.id = book.id;
-    td.classList.add("remove");
     tr.appendChild(td);
     tbody.appendChild(tr);
   });
+
+  let removeBookButtons = document.querySelectorAll(".remove");
+  removeBookButtons.forEach((button) => (button.onclick = () => {}));
 
   main.appendChild(table);
 }
@@ -68,20 +92,19 @@ function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-addBookToLibrary("Book 1", "Author 1", 1991);
-addBookToLibrary("Book 2", "Author 2", 1992);
+addBookToLibrary("Book 1", "Author 1", 1991, true);
+addBookToLibrary("Book 2", "Author 2", 1992, true);
 addBookToLibrary("Book 3", "Author 3", 1993);
 addBookToLibrary("Book 4", "Author 4", 1994);
 addBookToLibrary("Book 5", "Author 5", 1995);
 
 listBooks();
 
-// Button related code
+// Buttons related code
 const openModal = document.querySelector("[data-open-modal]");
 const closeModal = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-model]");
 const form = document.querySelector("form");
-let removeBookButtons = document.querySelectorAll(".remove");
 
 openModal.addEventListener("click", () => {
   modal.showModal();
@@ -113,15 +136,9 @@ form.addEventListener("submit", (e) => {
 
   author = author.length < 1 ? "unknown" : author;
   year = year.length < 1 ? "unknown" : year;
-});
 
-removeBookButtons.forEach(
-  (button) =>
-    (button.onclick = () => {
-      let buttonId = button.id;
-      const book = myLibrary.find((book) => book.id === buttonId);
-      const index = myLibrary.indexOf(book);
-      button.parentNode.remove();
-      myLibrary.splice(index, 1);
-    }),
-);
+  addBookToLibrary(title, author, year, read);
+  listBooks();
+  modal.close();
+  form.reset();
+});
